50de77abb9d5abcda8b739b3e17bd11dcd8e434d
IsacHolstad <wisac22@gmail.com>
2022-10-06 13:22:03 +0200

    made all requierd pages

--- js-ca/vite.config.js
+++ js-ca/vite.config.js
@@ -1,12 +1,20 @@
 // vite.config.js
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
 
 export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
+                home: resolve(__dirname, 'index.html'),
+                signup: resolve(__dirname, 'signup.html'),
+                login: resolve(__dirname, 'login.html'),
+                welcome: resolve(__dirname, 'welcome.html'),
+                createPosts: resolve(__dirname, 'creat-post.html'),
+                singlePosts: resolve(__dirname, 'single-post.html'),
+                editPosts: resolve(__dirname, 'edit-post.html'),
+
+
             }
         }
     }
