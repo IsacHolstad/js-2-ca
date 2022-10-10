ee40e160880d25e569f794d91c7e71c9cbf65740
IsacHolstad <wisac22@gmail.com>
2022-10-10 16:39:23 +0200

    making login form

---
+++ js-ca/js/login.js
@@ -0,0 +1,6 @@
+import {USER_LOGIN_URL} from "./settings/api.js";
+import {validateEmail} from "./utils/validation.js";
+import {saveUser, saveToken} from "./utils/storage.js";
+
+const logInForm = document.querySelector("#login-form");
+console.log(logInForm);
