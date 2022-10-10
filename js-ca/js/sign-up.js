a035f279f693d10b078c16834477819ad69468e9
IsacHolstad <wisac22@gmail.com>
2022-10-10 14:24:58 +0200

    selecting every elements in signup

--- js-ca/js/sign-up.js
+++ js-ca/js/sign-up.js
@@ -1,2 +1,20 @@
 import {USER_SIGNUP_URL} from "./settings/api.js";
-USER_SIGNUP_URL()
+import {validatePassword} from "./utils/validation.js";
+import {validateEmail} from "./utils/validation.js";
+
+const contactForm = document.querySelector("#signup-form");
+
+const firstName = document.querySelector("#firstName");
+const firstNameError = document.querySelector("#firstNameError")
+
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#emailError");
+const emailErrorNotValid = document.querySelector("#emailErrorNotValid");
+
+const password = document.querySelector("#password");
+const passwordError = document.querySelector("#passwordError");
+
+const confirmPassword = document.querySelector("#confirm_password");
+const confirmPasswordError = document.querySelector("#confirmPasswordError");
+
+
