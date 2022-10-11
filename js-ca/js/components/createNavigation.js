5cc3f91c9501dc20b6271a70815840cb5c87e3f9
IsacHolstad <wisac22@gmail.com>
2022-10-11 12:39:28 +0200

    hover and other styling for navigation

--- js-ca/js/components/createNavigation.js
+++ js-ca/js/components/createNavigation.js
@@ -6,19 +6,19 @@ function createNavigation() {
     if (navBar) {
         const userName = getUserName();
         let navLinks;
-        navLinks = `<li class="p-8"><a href="/signup.html" class="${pathname === "signup.html" ? "text-blue-900" : ""}"</a>Sign Up </li>
-                    <li class="p-8"><a href="/login.html" class="${pathname === "login.html" ? "text-blue-900" : ""}"</a>Login  </li>`;
+        navLinks = `<li class="p-8"><a href="/signup.html" class="${pathname === "signup.html" ? "text-blue-900" : "hover:underline"}"</a>Sign Up </li>
+                    <li class="p-8"><a href="/login.html" class="${pathname === "login.html" ? "text-blue-900" : "hover:underline"}"</a>Login  </li>`;
         if (userName) {
             navLinks = `
-            <li class="p-8"><a href="index.html" class="${pathname === "/index.html" ? "text-blue-900" : ""}">Home</a>
+            <li class="p-8"><a href="index.html" class="${pathname === "/index.html" ? "" : " hover:underline"}">Home</a>
             </li>
             <li class="p-8">
-                <a href="/create-post.html" class="${pathname === "create-post.html" ? "text-blue-900" : ""}">Create Post</a>
+                <a href="/create-post.html" class="${pathname === "create-post.html" ? "" : "hover:underline"}">Create Post</a>
             </li>
-            <li class="p-8"><a href="./my-posts.html" class="${pathname === "my-posts.html" ? "text-blue-900" : ""}">My Posts</a>
+            <li class="p-8"><a href="./my-posts.html" class="${pathname === "my-posts.html" ? "" : "hover:underline"}">My Posts</a>
             </li>
-            <li class="p-8"><span>Hey 👋 ${userName}</span></li>
-            <li class="p-8"><button id="logout-button" class="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded-full">
+            <li class="p-8"><span class="hover:underline">Hey 👋 ${userName}</span></li>
+            <li class="p-8"><button id="logout-button" class="text-red-500 hover:underline">
             Logout
             </button></li>
             `
