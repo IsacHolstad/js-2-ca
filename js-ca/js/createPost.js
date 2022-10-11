b19ab26cb7055e293181d0e8fd341fc68e1bee5f
IsacHolstad <wisac22@gmail.com>
2022-10-11 14:22:59 +0200

    miner typo errors and element checks

--- js-ca/js/createPost.js
+++ js-ca/js/createPost.js
@@ -1,2 +1,14 @@
 import {getToken} from "./utils/storage.js";
 import {GET_POSTS_URL} from "./settings/api.js";
+const createPostForm = document.querySelector("#create-post-form");
+
+const postTitle = document.querySelector("#post-title");
+const postTitleError = document.querySelector("#postTitleError");
+
+const postDescription = document.querySelector("#postDescription");
+const postDescriptionError = document.querySelector("#postDescriptionError");
+
+
+
+console.log("Every element: ",postDescriptionError, postDescription, postTitleError, postTitle, createPostForm );
+
