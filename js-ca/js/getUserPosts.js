b1b54b8224848f242c2e675f304d4c3f1b1b3250
IsacHolstad <wisac22@gmail.com>
2022-10-11 16:54:01 +0200

    my posts getting my posts data in console

--- js-ca/js/getUserPosts.js
+++ js-ca/js/getUserPosts.js
@@ -1,3 +1,37 @@
 import moment from "moment";
 import {GET_USER_POSTS_URL, DELETE_USER_POST_BY_ID} from "./settings/api"
 import {getToken} from "./utils/storage";
+
+let now = moment(new Date());
+const accessToken = getToken();
+
+const postsContainer = document.querySelector("#posts-container");
+const postsNotificationMessage = document.querySelector(".posts__notification");
+
+async function getMyPosts() {
+    const response = await fetch(GET_USER_POSTS_URL, {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${accessToken}`
+        }
+    })
+    if (response.ok) {
+        const jsonResponse = await response.join();
+        console.log("got my post in log");
+        postsContainer.innerHTML = "";
+        const {posts} = jsonResponse;
+        if (!posts.length) {
+            postsNotificationMessage.innerHTML = "Your Feed Is Empty";
+        } else {
+            const numberOfPosts = posts.length;
+            for (let i = 0; i < numberOfPosts; i++) {
+                const {created} = posts[i];
+                console.log(posts[i])
+                const minutesSinceCreated = now.diff(created, "minutes");
+                postsContainer.innerHTML += `
+                
+                `
+            }
+        }
+    }
+}
