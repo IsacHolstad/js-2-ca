4f566545b69a9986f052d79542597c8b68a90b04
IsacHolstad <wisac22@gmail.com>
2022-10-11 19:02:54 +0200

    bugg hunting delete button

--- js-ca/js/getUserPosts.js
+++ js-ca/js/getUserPosts.js
@@ -33,7 +33,7 @@ async function getMyPosts() {
                 <div class=" flex items-center justify-center  container drop-shadow-md py-2 ">
                   <div class="px-5 py-4 bg-white dark:bg-gray-800 shadow rounded-lg container ">
                     <div class="flex mb-4">
-                      <img class="w-12 h-12 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"/>
+                      <img class="w-12 h-12 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="some randome women"/>
                       <div class="ml-2 mt-0.5">
                         <span class="block font-medium text-base leading-snug text-black dark:text-gray-100">${posts[i].title}</span>
                       </div>
@@ -56,13 +56,69 @@ async function getMyPosts() {
                     <div class="ml-1 text-gray-500 dark:text-gray-400 font-light">${minutesSinceCreated} m ago</div>
                     </div>
                   </div>
-                </div>
-               
-                     
-                
-                `
+                </div>`
             }
         }
+    } else {
+        postsNotificationMessage.innerHTML = await response.json()
+        console.log("retriving post failed")
     }
 }
-getMyPosts();
+getMyPosts().then(() =>{
+    handleDeleteBtnsEvent()
+})
+
+function handleDeleteBtnsEvent() {
+    let deleteButtons = document.getElementsByClassName('delete-post-button');
+    console.log(deleteButtons)
+
+    const totalNumbersOfDeleteBtns = deleteButtons.length;
+    for (let i = 0; i < totalNumbersOfDeleteBtns; i++) {
+        console.log(i);
+        deleteButtons[i].addEventListener('click', function () {
+            console.log(`${i} hey i triggerd the click envent`)
+            console.log("this.dataset.postId: ", this.getAttribute("data-id"))
+            console.log(this.dataset)
+
+            const postId = this.dataset.id;
+            handleDeletePostById(postId);
+        });
+    }
+
+
+}
+
+function handleDeletePostById(id) {
+    console.log(id)
+    const deleteUserById = async () => {
+        try{
+            let response = await fetch(`${DELETE_USER_POST_BY_ID}/${id}`,{
+
+                method: "DELETE",
+                headers: {
+                    "Authorization": `Bearer ${accessToken}`
+                }
+
+
+            });
+            if (response.status === 200){
+                console.log("deleted post button clicked");
+                getMyPosts().then(() => {
+                    handleDeleteBtnsEvent()
+                });
+
+
+
+            } else {
+                const err = await response.json()
+                const message = `Sorry post could not be deleted ${err}`
+                throw Error(message)
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+deleteUserById().then(r =>{
+
+    })
+}
