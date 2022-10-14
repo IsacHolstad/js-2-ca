3685ba05b29456abea4b95e8a252ffd7f26288c5
IsacHolstad <wisac22@gmail.com>
2022-10-14 14:06:01 +0200

    edit post is working :)

--- js-ca/js/editPostById.js
+++ js-ca/js/editPostById.js
@@ -11,6 +11,7 @@ const postDescriptionError = document.querySelector("#postDescriptionError");
 
 
 
+
 const paramString = window.location.search
 const searchParam = new URLSearchParams(paramString);
 const postId = searchParam.get("post_id");
@@ -19,7 +20,7 @@ const postId = searchParam.get("post_id");
 async function getPostById() {
     const response = await fetch(`${GET_POST_BY_ID_URL}/${postId}`, {
         method: "GET",
-        headers : {
+        headers: {
             "Content-Type": "application/json",
             "Authorization": `Bearer ${accessToken}`
         }
@@ -34,32 +35,27 @@ async function getPostById() {
         postDescription.value = body;
     } else{
         const err = await response.json();
-        throw err.message
+        throw err.message;
     }
 }
-getPostById().catch( err => {
+getPostById().catch(err => {
     console.log(err)
 });
-console.log(isPostTitle)
 
 editPostForm.addEventListener("submit", function (event) {
     event.preventDefault();
     let isPostTitle = false;
-
-    //TODO cannot read trim() method
-
-    if (isPostTitle.value.trim().length > 0) {
+    if (postTitle.value.trim().length > 0) {
         postTitleError.classList.add("hidden");
-        isPostTitle = true
-    } else{
+        isPostTitle = true;
+    } else {
         postTitleError.classList.remove("hidden")
     }
 
     let isPostDescription = false;
 
-    //TODO cannot read trim() method
 
-    if (isPostDescription.value.trim().length > 0) {
+    if (postDescription.value.trim().length > 0) {
         postDescriptionError.classList.add("hidden")
         isPostDescription = true;
     } else {
@@ -70,32 +66,31 @@ editPostForm.addEventListener("submit", function (event) {
 
     if (isFormValid) {
         console.log("success")
-        console.log("hehhejedjejdjejkffkkfkfk", postTitle.value)
         console.log(postDescription)
         const postData = {
-            "title" : postTitle.value,
+            "title": postTitle.value,
             "body": postDescription.value
         };
         console.log(postData)
-        const accessToken = getToken()
-        console.log(accessToken)
-        (async function editPost() {
+        const accessToken = getToken();
+
+
+
+        (async function createPost() {
             const response = await fetch(`${EDIT_POST_URL}/${postId}`, {
                 method: "PUT",
-                headers :{
-                    "Content-type": "application/json",
+                headers: {
+                    "Content-Type": "application/json",
                     "Authorization": `Bearer ${accessToken}`
                 },
                 body: JSON.stringify(postData)
             })
-            console.log(response)
             if (response.ok) {
-                const data = await response.json()
-                console.log(data)
+                const data = await response.json();
+                console.log(data);
                 console.log("post has been edited")
                 location.href = `single-post.html?post_id=${postId}`
-            }
-            else {
+            } else {
                 const err = await response.json();
                 const message = "Edit post failed"
                 throw new Error(message)
@@ -103,9 +98,9 @@ editPostForm.addEventListener("submit", function (event) {
             editPostForm.reset();
         })().catch(err => {
             console.log(err)
-        })
+        });
     }else {
         console.log("validation failed")
     }
+
 })
-console.log(editPostForm)
